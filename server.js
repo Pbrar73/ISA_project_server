14fd991f4dc48542a0e0212c3fcc9bdf63b9c2a2
Jaskaran Toor <jaskarantoor00@gmail.com>
2024-04-06 17:55:33 -0700

    update 7

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://regal-axolotl-938764.netlify.app');
+    const allowedOrigins = ['https://regal-axolotl-938764.netlify.app', 'http://localhost:3000']; // Add your client application's origin here
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -23,6 +27,7 @@ app.use((req, res, next) => {
     }
     next();
 });
+
 
 const dbUrl = process.env.JAWSDB_URL;
 let dbOptions = {};
